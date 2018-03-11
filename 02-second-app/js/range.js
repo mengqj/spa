4cbcc14cd30e3c0c79e7ab45a96f8be49f70e445
孟秋建 <2931602191@qq.com>
2018-03-11 16:43:18 +0800

    Update range.js

--- 02-second-app/js/range.js
+++ 02-second-app/js/range.js
@@ -5,5 +5,5 @@ function myFunction()
   //console.log(document.getElementById('range').value);
   //spantext.innerHTML='';
   var x=document.getElementById('range').value;
-  document.getElementByid('text').innerHTML='你的年龄是:'+x+'岁'; 
+  document.getElementByid('text').innerHTML='你的年龄是:'+ x +'岁'; 
 }
